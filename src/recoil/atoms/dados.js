08f64e8006c955649d70702021bd4f0cea076aec
joao <zala4453523@gmail.com>
2023-08-25 09:31:24 -0300

    V7

--- src/recoil/atoms/dados.js
+++ src/recoil/atoms/dados.js
@@ -1,10 +1,13 @@
 import { atom } from "recoil";
 
 export const dadosState = atom({
-  key: "dados",
+  key: "dadosEstado",
   default: {
     categorias: [],
     marcas: [],
     produtos: [],
+    loggedIn: false,
+    access_token: null,
+    refresh_token: null,
   },
 });
--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,35 @@ import React from "react";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
 import { MaterialIcons } from "@expo/vector-icons";
+import { useRecoilValue } from "recoil";
 
 import Home from "./screens/Home";
+import Login from "./src/screens/Login/index.js";
 import Perfil from "./screens/Perfil";
 
 const BottomTab = createBottomTabNavigator();
+const LoginStack = createStackNavigator();
 
+import { dadosState } from "../src/recoil/atoms/dados.js";
+const currentAuthState = useRecoilValue(dadosState);
+
+function LoginConfirm() {
+  <NavigationContainer>
+    <LoginStack.Navigator>
+      {currentAuthState.loggedIn ? (
+        <Stack.Screen name="Home" component={Home} />
+      ) : (
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+      )}
+    </LoginStack.Navigator>
+  </NavigationContainer>;
+}
 export default function Routes() {
   return (
     <NavigationContainer>
